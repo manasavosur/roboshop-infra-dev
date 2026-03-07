b2ec1998fe73f692ea2e4307a073fbd793d09066
Manasa <manasa.vosur@gmail.com>
2026-03-07 22:14:29 +0530

    roboshop infra dev

---
+++ 40-databases/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z0439877UYVGSF6PAFL4"
+}
+
+variable "domain_name" {
+    default = "chotu.online"
+}
