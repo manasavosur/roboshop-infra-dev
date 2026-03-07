1f94bae27a09720b22ca198d35eb2117335335f4
Manasa <manasa.vosur@gmail.com>
2026-03-08 00:25:15 +0530

    roboshop infra dev

---
+++ destroy.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e
+
+echo "Destroying Databases..."
+cd 40-databases
+terraform destroy -auto-approve
+echo "Remaining resources:"
+terraform state list || echo "No resources left"
+cd ..
+
+echo "Destroying Bastion..."
+cd 30-bastion
+terraform destroy -auto-approve
+echo "Remaining resources:"
+terraform state list || echo "No resources left"
+cd ..
+
+echo "Destroying SG Rules..."
+cd 20-sg-rules
+terraform destroy -auto-approve
+echo "Remaining resources:"
+terraform state list || echo "No resources left"
+cd ..
+
+echo "Destroying Security Groups..."
+cd 10-sg
+terraform destroy -auto-approve
+echo "Remaining resources:"
+terraform state list || echo "No resources left"
+cd ..
+
+echo "Destroying VPC..."
+cd 00-vpc
+terraform destroy -auto-approve
+echo "Remaining resources:"
+terraform state list || echo "No resources left"
+cd ..
+
+echo "All infrastructure destroyed."
