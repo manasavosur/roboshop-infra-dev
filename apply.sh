895b14262b5755915d59960456fe2b04df8556df
Manasa <manasa.vosur@gmail.com>
2026-03-12 16:43:35 +0530

    roboshop infra dev

--- apply.sh
+++ apply.sh
@@ -6,7 +6,6 @@ for dir in 00-vpc 10-sg 20-sg-rules 30-bastion 50-backend-alb
 do
   echo "Applying Terraform in $dir..."
   cd $dir
-  terraform init -reconfigure
   terraform apply -auto-approve
   cd ..
 done
