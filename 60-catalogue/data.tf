94e999da2ff4734a10bbd32e1706466b16ba6e5f
Manasa <manasa.vosur@gmail.com>
2026-03-13 14:24:30 +0530

    roboshop infra dev

--- 60-catalogue/data.tf
+++ 60-catalogue/data.tf
@@ -30,6 +30,3 @@ data "aws_ssm_parameter" "private_subnet_ids" {
     name = "/${var.project}/${var.environment}/private_subnet_ids"
 }
 
-data "aws_ssm_parameter" "backend_alb_listener_arn" {
-    name = "/${var.project}/${var.environment}/backend_alb_listener_arn"
-}
