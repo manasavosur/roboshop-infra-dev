17cbc5187726d841741ee3b0f11f479737aab85b
Manasa <manasa.vosur@gmail.com>
2026-03-13 15:15:29 +0530

    roboshop infra dev

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -5,6 +5,7 @@ environment=$2
 dnf install ansible -y
 
 cd /home/ec2-user
+rm -rf ansible-roboshop-roles-tf
 git clone https://github.com/manasavosur/ansible-roboshop-roles-tf
 
 cd ansible-roboshop-roles-tf
