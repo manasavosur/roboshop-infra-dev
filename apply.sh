222fb520058fbb6a548147384f221d9494bf03d5
Manasa <manasa.vosur@gmail.com>
2026-03-19 15:09:43 +0530

    roboshop infra dev

--- apply.sh
+++ apply.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
-
 set -e
 
-for dir in 00-vpc 10-sg 20-sg-rules 30-bastion 50-backend-alb
+for dir in 00-vpc 10-sg 20-sg-rules 30-bastion
 do
-  echo "Applying Terraform in $dir..."
+  echo "Applying $dir..."
   cd $dir
   terraform apply -auto-approve
   cd ..
 done
 
-echo "Infrastructure creation complete."
+# Apply DB separately
+echo "Applying databases..."
+cd 40-databases
+terraform apply -auto-approve
+cd ..
+
+# Continue rest
+for dir in 50-backend-alb 60-catalogue 70-acm 80-frontend-alb
+do
+  echo "Applying $dir..."
+  cd $dir
+  terraform apply -auto-approve
+  cd ..
+done
