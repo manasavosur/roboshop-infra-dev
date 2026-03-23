562d1afbb3b320235570bbd565b7db9ff9dc862b
Manasa <manasa.vosur@gmail.com>
2026-03-23 17:02:10 +0530

    roboshop infra dev

--- apply.sh
+++ apply.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -e
 
-for dir in 00-vpc 10-sg 20-sg-rules 30-bastion
-do
-  echo "Applying $dir..."
-  cd $dir
-  terraform apply -auto-approve
-  cd ..
-done
+# for dir in 00-vpc 10-sg 20-sg-rules 30-bastion
+# do
+#   echo "Applying $dir..."
+#   cd $dir
+#   terraform apply -auto-approve
+#   cd ..
+# done
 
 #ssh to bastion and run terraform init and apply for 40-databases manually as they are using private ips
 
-# Continue rest
+Continue rest
 for dir in 50-backend-alb 70-acm 80-frontend-alb
 do
   echo "Applying $dir..."
