be3a1aaadc1eca9cc2d349edcb5f0a4111eaad18
Manasa <manasa.vosur@gmail.com>
2026-03-24 15:38:58 +0530

    roboshop infra dev

--- apply.sh
+++ apply.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 set -e
 
-# for dir in 00-vpc 10-sg 20-sg-rules 30-bastion
-# do
-#   echo "Applying $dir..."
-#   cd $dir
-#   terraform apply -auto-approve
-#   cd ..
-# done
-
-#ssh to bastion and run terraform init and apply for 40-databases manually as they are using private ips
-
-# Continue rest
-for dir in 50-backend-alb 70-acm 80-frontend-alb
+for dir in 00-vpc 10-sg 20-sg-rules 30-bastion
 do
   echo "Applying $dir..."
   cd $dir
@@ -20,4 +9,15 @@ do
   cd ..
 done
 
-echo "Infrastructure creation completed successfully."
+#ssh to bastion and run terraform init and apply for 40-databases manually as they are using private ips
+
+# Continue rest
+# for dir in 50-backend-alb 70-acm 80-frontend-alb
+# do
+#   echo "Applying $dir..."
+#   cd $dir
+#   terraform apply -auto-approve
+#   cd ..
+# done
+
+# echo "Infrastructure creation completed successfully."
